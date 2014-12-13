b4e76292d695dab20f87b0b06ecdb4d3dbc6c742
Neta Zmora <neta.zmora@intel.com>
2014-12-13 17:35:36 +0200

    added Prewitt operator

--- kernels/cv.cpp
+++ kernels/cv.cpp
@@ -31,11 +31,51 @@ std::pair<Halide::Func, Halide::Func> scharr_3x3(Halide::Func input, bool graysc
     return ret;
 }
 
-Halide::Func magnitude(Halide::Func gx, Halide::Func gy) {
+// Prewitt operator
+// Computes x,y gradients
+// http://en.wikipedia.org/wiki/Prewitt_operator
+std::pair<Halide::Func, Halide::Func> prewitt_3x3(Halide::Func input, bool grayscale) {
+    Halide::Func kx("kx"), ky("ky");
+    Halide::Func gx("gradient_x"), gy("gradient_y");
+    Halide::RDom r(-1,3,-1,3);
+    Halide::Var x,y,c;
+    
+    kx(x,y) = 0;
+    kx(-1,-1) = -1;    kx(0,-1) = 0;    kx(1,-1) = 1;
+    kx(-1, 0) = -1;    kx(0, 0) = 0;    kx(1, 0) = 1;
+    kx(-1, 1) = -1;    kx(0, 1) = 0;    kx(1, 1) = 1;
+    if (grayscale)
+        gx(x,y) = sum(input(x+r.x, y+r.y) * kx(r.x, r.y));
+    else
+        gx(x,y,c) = sum(input(x+r.x, y+r.y, c) * kx(r.x, r.y));
+        
+    ky(x,y) = 0;
+    ky(-1,-1) = -1;    ky(0,-1) = -1;    ky(1,-1) = -1;
+    ky(-1, 0) =  0;    ky(0, 0) =  0;    ky(1, 0) =  0;
+    ky(-1, 1) =  1;    ky(0, 1) =  1;    ky(1, 1) =  1;
+    if (grayscale)
+        gy(x,y) = sum(input(x+r.x, y+r.y) * ky(r.x, r.y));
+    else
+        gy(x,y,c) = sum(input(x+r.x, y+r.y, c) * ky(r.x, r.y));
+
+    std::pair<Halide::Func, Halide::Func> ret = std::make_pair(gx, gy);
+    return ret;
+}
+
+// compute gradient magnitude
+Halide::Func magnitude(Halide::Func Gx, Halide::Func Gy) {
     Halide::Func mag;
     Halide::Var x,y,c;
     mag(x,y) =  Halide::sqrt( 
-                     Halide::pow(gx(x,y), 2) + Halide::pow(gy(x,y), 2)
+                     Halide::pow(Gx(x,y), 2) + Halide::pow(Gy(x,y), 2)
                 );
+    return mag;
+}
+
+// compute gradient angle
+Halide::Func angle(Halide::Func Gx, Halide::Func Gy) {
+    Halide::Func mag;
+    Halide::Var x,y,c;
+    mag(x,y) =  Halide::atan2(Gx(x,y), Gy(x,y));
     return mag;
 }
--- kernels/cv.h
+++ kernels/cv.h
@@ -4,6 +4,7 @@
 #include "Halide.h"
 
 std::pair<Halide::Func, Halide::Func> scharr_3x3(Halide::Func input, bool grayscale = false);
+std::pair<Halide::Func, Halide::Func> prewitt_3x3(Halide::Func input, bool grayscale = false);
 Halide::Func magnitude(Halide::Func gx, Halide::Func gy);
 
 #endif // __CV_H
